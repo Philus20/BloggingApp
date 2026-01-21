77fcb7f750c32b690a3d17b0ead01f0073d9844d
TheophilusQuaicoe <philus0026@gmail.com>
2026-01-21 05:17:43 +0000

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module org.example.bloggingapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.sql;
+    requires org.example.bloggingapp;
+
+    opens org.example.bloggingapp to javafx.fxml;
+    exports org.example.bloggingapp;
+}
